ce12bae8cd45d47ca31a030ef4f8445c0d474c06
fukuda56513 <fukuda56513@gmail.com>
2015-01-20 12:24:02 +0900

    uuuu

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113035734) do
+ActiveRecord::Schema.define(version: 20150120030952) do
 
   create_table "comments", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "game_id"
   end
 
   create_table "games", force: true do |t|
